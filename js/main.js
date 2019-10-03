d381a1428431fab617efb9422ecf98863f474f72
DenisKen <denis.kenno@gmail.com>
2019-10-03 14:36:23 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //INIT
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100000 );
 camera.position.set( 0, 0, 200 );
 
 
